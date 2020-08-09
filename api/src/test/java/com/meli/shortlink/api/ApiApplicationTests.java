ea7e8330c7365959b005d9d1d16ee9c986e0dd27
jvaleriano <valeriano.javier@gmail.com>
2020-08-09 15:15:15 -0300

    Initial commit

---
+++ api/src/test/java/com/meli/shortlink/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.shortlink.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
