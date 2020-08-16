b889b2faca2a7e2648a079d24c88a392db56533c
jvaleriano <valeriano.javier@gmail.com>
2020-08-16 18:43:00 -0300

    change port remove unused code

--- server/src/test/java/com/meli/shortlink/server/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.meli.shortlink.server;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
